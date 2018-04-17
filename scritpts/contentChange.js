c45ae5aeff792d92867547a06d78b3107869b2f4
Kasper Devicki <kasper.devicki@gmail.com>
2018-04-17 15:52:08 +0200

    finished version 1.0.0

--- scritpts/contentChange.js
+++ scritpts/contentChange.js
@@ -1,25 +1,37 @@
-let tabs = $("[id*='tabs-']");
-let links = $('#tabs a');
-let counter = $('.counter')
-
-tabs.hide();
-tabs[0].style.display = "block";
-
-links.click(function (e) {
-    let target = e.target;
-    let targetIdText = target.attributes.href.textContent;
-
-
-    tabs.each(function (i) {
-        if (targetIdText === tabs[i].attributes.id.value) {
-            $(this).slideDown(600);
-
-            let counterText = targetIdText.split('')
-            counterNumber = counterText[counterText.length - 1];
-            counter.text('0' + counterNumber);
-
-        } else {
-            $(this).hide(600);
-        }
+(function () {
+    $('.right-side').hide().delay(400).animate({
+        height: '100%'
+    }).show();
+    $('.right-side .bottom').animate({
+        width: '100%'
     });
-})
+
+    $('.slide-in, .counter, nav').hide().slideDown(1400, function () {
+        $('.content').fadeIn();
+    });
+})();
+
+(function () {
+    let tabs = $("[id*='tabs-']");
+    let links = $('#tabs a');
+    let counter = $('.counter')
+
+
+    tabs.hide();
+    tabs[0].style.display = "block";
+
+    links.click(function (e) {
+        let target = e.target;
+        let targetIdText = target.attributes.href.textContent;
+        tabs.each(function (i) {
+            if (targetIdText === tabs[i].attributes.id.value) {
+                $(this).slideDown(600);
+                let counterText = targetIdText.split('')
+                counterNumber = counterText[counterText.length - 1];
+                counter.text('0' + counterNumber);
+            } else {
+                $(this).hide(600);
+            }
+        });
+    })
+})();
